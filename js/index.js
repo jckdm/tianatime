b7c4034444a7e99bf1bd78792eb5be6d85078513
Jack Adam <jackadam@Jacks-MBP-3.home>
2020-04-05 19:24:35 -0400

    we just shook maracas out the windows and i've seen the faces of neighbors i didnt know existed. nothing ever happens on my block.

--- js/index.js
+++ js/index.js
@@ -4,10 +4,10 @@ function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
 window.onload = function() { color(); };
 
 async function color() {
+  var x = document.getElementsByTagName('a')[0];
+  var y = document.getElementsByTagName('a')[1];
+  var z = document.getElementsByTagName('a')[2];
   while (true) {
-    var x = document.getElementsByTagName('a')[0];
-    var y = document.getElementsByTagName('a')[1];
-    var z = document.getElementsByTagName('a')[2];
     var d = new Date();
     var h = d.getHours();
     var m = d.getMinutes();
@@ -16,11 +16,11 @@ async function color() {
     // 7am – 7pm
     if (h >= 7 && h < 19) { hrs = 18 - h; }
     // 7pm – 7am
-    if (h >= 19 || h < 7) { hrs = (h > 19) ? 7 - (23 - h) : 7 - h; }
-    var c = (((hrs * 60) + (60 - m)) / 1440) * 255;
+    if (h >= 19 || h < 7) { hrs = (h >= 19) ? 7 + (23 - h) : 7 - h; }
+    var c = (((hrs * 60) + (60 - m)) / 720) * 255;
     var cc = 255 - c;
 
-    if (h >= 7 && h < 19) { [c, cc] = [cc, c]; }
+    if (h >= 7 && h < 19) { [c, cc] = [cc, c]; } // white --> black
 
     document.body.style.backgroundColor = 'rgb(' + cc + ',' + cc + ',' + cc + ')';
     x.style.color = y.style.color = z.style.color = 'rgb(' + c + ',' + c + ',' + c + ')';
