853ff19b7c53da61ad56a05e9e57a9abb1ed426c
Jack Adam <jackadam@Jacks-MBP-3.home>
2020-04-08 19:59:52 -0400

    fuk this is broken on chrome, fuck google tho

--- js/full.js
+++ js/full.js
@@ -1,24 +1,12 @@
-window.onload = function() { $('#container').flexgal(); color(); };
-
 (function($) {
-  $.fn.flexgal = function(){
-    $('body').prepend('<div id="fullimage" style="display: none"></div>')
+  $.fn.flexgal = function() {
+    $('body').prepend('<div id="fullimage" style="display: none"></div>');
     $(this).addClass('flex-gallery');
-    $('img', this).parent().addClass('image-rate');
-
-   $('.image-rate').click(function() {
-     $('img', this).clone().prependTo('#fullimage');
-     $("#fullimage").fadeIn(200);
-   });
-
-   $('#fullimage').click(function() {
-     $(this).fadeOut(200, function() {
-       $('img',this).remove();
-     });
-   });
+    $('img',this).parent().addClass('image-rate');
  }
 }(jQuery));
 
+window.onload = function() { $('#container').flexgal(); color(); };
 function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
 
 async function color() {
