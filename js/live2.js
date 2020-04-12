ac9a1f70718fd30b112d838561496244c4287035
Jack Adam <jackadam@Jacks-MBP-3.home>
2020-04-12 02:06:14 -0400

    full rotation 1 – 180 from 7am to 7am

--- js/live2.js
+++ js/live2.js
@@ -6,7 +6,13 @@ async function exhibit() {
   var d = new Date();
   var h = d.getHours();
   var m = d.getMinutes();
-  var i = 1;
+  var hrs;
+
+  // 7am – 12am
+  if ((h >= 7 && h < 19) || (h >= 19)) { hrs = 7 + (23 - h); }
+  // 12am – 7am
+  if (h < 7) { hrs = 6 - h; }
+  var i = 180 - (Math.floor((((hrs * 60) + (60 - m)) / 1440) * 180));
   while (true) {
     $('.epix').css('display','none');
     document.getElementById(String(Math.max(1, i%181))).style.display = 'block';
