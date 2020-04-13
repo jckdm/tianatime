be3a0342ff7be9e4838acfe04b876cdd2a93b945
Jack Adam <jackadam@Jacks-MBP-3.home>
2020-04-12 22:53:37 -0400

    started the archive

--- js/archive.js
+++ js/archive.js
@@ -22,8 +22,6 @@ $(function() {
   color();
 })
 
-function sleep(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
-
 async function color() {
   while (true) {
     var d = new Date();
@@ -34,13 +32,14 @@ async function color() {
     // 7am – 7pm
     if (h >= 7 && h < 19) { hrs = 18 - h; }
     // 7pm – 7am
-    if (h >= 19 || h < 7) { hrs = (h >= 19) ? 7 + (23 - h) : 7 - h; }
+    if (h >= 19 || h < 7) { hrs = (h >= 19) ? 7 + (23 - h) : 6 - h; }
     var c = (((hrs * 60) + (60 - m)) / 720) * 255;
     var cc = 255 - c;
 
     if (h >= 7 && h < 19) { [c, cc] = [cc, c]; } // white --> black
 
     $("body").css("backgroundColor", 'rgb(' + cc + ',' + cc + ',' + cc + ')');
+    $('p').css('color', 'rgb(' + c + ',' + c + ',' + c + ')');
     await sleep(60000);
   }
 }
