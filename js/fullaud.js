1c2814f5b994e9504ff0f71077e5db562da53c8d
Jack Adam <jackadam@Jacks-MBP-3.home>
2020-04-18 19:26:41 -0400

    update audio icon thanks tiana

--- js/fullaud.js
+++ js/fullaud.js
@@ -1,17 +1,15 @@
 var a = document.getElementById('audio');
-var ad = document.getElementById('aud');
-
 var on = false;
 
 async function play() {
   if (on == true) {
     on = false;
     a.pause();
-    ad.style.color = "#3CB371";
+    $('#aud').attr('src','vol-red.png');
   }
   else {
     on = true;
-    ad.style.color = "#FF6F61";
+    $('#aud').attr('src','vol-green.png');
     a.play();
     await sleep(a.duration * 1000);
     a.pause();
