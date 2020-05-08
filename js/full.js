4300455cc08cef772d33d32f0f62ba04307a94f2
Jack Adam <jackadam@Jacks-MBP-3.home>
2020-05-08 00:32:05 -0400

    staging site for screenshots hehe

--- js/full.js
+++ js/full.js
@@ -55,8 +55,8 @@ async function color() {
     if (h >= 6 && h < 22) { bg = "#FFFFFF"; fg = "#000000"; }
     if (h >= 22 || h < 6) { bg = "#000000"; fg = "#FFFFFF"; }
 
-    $('body').css("backgroundColor", bg);
-    $('p').css('color', fg);
+    $('body').css("backgroundColor", fg);
+    $('p').css('color', bg);
     await sleep(60000);
   }
 }
